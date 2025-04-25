2bb12e955f79e1b8cde40ed3f6aec535f8269ff0
Maria-Eduarda-JSantos <mariaeduardasj20@outlook.com>
2025-04-25 13:51:18 -0300

    Back corrigido problema de Cors, rota e api

--- Teste_tasks/Program.cs
+++ Teste_tasks/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Identity;
+Ôªøusing Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -6,100 +6,106 @@ using Teste_tasks.Data;
 using Teste_tasks.Models;
 using Teste_tasks.Services;
 
-
-
-
 namespace Teste_tasks
 {
 	public class Program
 	{
-
 		public static void Main(string[] args)
 		{
-
 			var builder = WebApplication.CreateBuilder(args);
 
-
+			// 1) CORS (sua origem React)
 			builder.Services.AddCors(options =>
 			{
-				options.AddPolicy("AllowAll", policy =>
+				options.AddPolicy("AllowFrontend", policy =>
 				{
-					policy.AllowAnyOrigin()
-						  .AllowAnyHeader()
-						  .AllowAnyMethod();
+					policy.WithOrigins(
+							"http://localhost:5173",
+							"http://localhost:5174"   // ‚Üê adiciona esta linha
+						)
+						.AllowAnyHeader()
+						.AllowAnyMethod()
+						.AllowCredentials();
 				});
 			});
 
+
+			// 2) DbContext
+			builder.Services.AddDbContext<AppDbContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+			// 3) Identity + EF stores + Token Providers
+			builder.Services.AddIdentity<Users, IdentityRole>(options =>
+			{
+				// Suas regras de senha e confirma√ß√£o
+				options.Password.RequireNonAlphanumeric = true;
+				options.Password.RequiredLength = 8;
+				options.Password.RequireUppercase = true;
+				options.Password.RequireLowercase = true;
+				options.User.RequireUniqueEmail = true;
+				options.SignIn.RequireConfirmedAccount = true;
+				options.SignIn.RequireConfirmedEmail = true;
+				options.SignIn.RequireConfirmedPhoneNumber = false;
+			})
+			.AddEntityFrameworkStores<AppDbContext>()
+			.AddDefaultTokenProviders();
+
+			// 4) Configura√ß√£o de Cookie e intercepta√ß√£o do RedirectToLogin
+			builder.Services.ConfigureApplicationCookie(options =>
+			{
+				// Permite envio em HTTP local e em fetch cross-site
+				options.Cookie.SameSite = SameSiteMode.None;
+				options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+
+				options.Events.OnRedirectToLogin = context =>
+				{
+					// Se for chamada √† API, devolve 401 em vez de redirect
+					if (context.Request.Path.StartsWithSegments("/api"))
+					{
+						context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+						return Task.CompletedTask;
+					}
+					// Sen√£o, redireciona normalmente
+					context.Response.Redirect(context.RedirectUri);
+					return Task.CompletedTask;
+				};
+			});
+
+			// 5) Servi√ßos de e-mail (SendGrid / SMTP)
+			builder.Services.Configure<AuthMessageSenderOptions>(
+				builder.Configuration.GetSection("AuthMessageSenderOptions"));
+			builder.Services.AddTransient<ICustomEmailSender, EmailSender>();
+
+			// 6) Controllers e Swagger
+			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
-			//builder.Services.AddTransient<ICustomEmailSender, EmailSender>();
-			//builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration.GetSection("SendGrid"));
-
-			//// Adicione o serviÁo de email
-			//builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration.GetSection("AuthMessageSenderOptions"));
-			//builder.Services.AddTransient<ICustomEmailSender, EmailSender>();
-
-			// para SMTP (confirmaÁ„o de registro)
-			builder.Services.Configure<AuthMessageSenderOptions>(
-				builder.Configuration.GetSection("AuthMessageSenderOptions"));
-
-			// para SendGrid (reset de senha)
-			builder.Services.AddTransient<ICustomEmailSender, EmailSender>();
-
-			//builder.Services.AddTransient<UserDeletionService>();
-
-			// Add services to the container.
-			builder.Services.AddControllers();
-			builder.Services.AddDbContext<AppDbContext>(Options =>
-			Options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-
-			//builder.Services.AddDbContext<AppDbContext>(Options =>
-			//Options.UseSqlServer(builder.Configuration.GetConnectionString("Default"), sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
-			// maxRetryCount: 3,                // N˙mero de tentativas de reconex„o
-			// maxRetryDelay: TimeSpan.FromSeconds(5), // Tempo m·ximo entre as tentativas
-			// errorNumbersToAdd: null          // Erros adicionais para tratar como transitÛrios (opcional)
-			//)));
-
-			builder.Services.AddIdentity<Users, IdentityRole>(Options =>
-			{
-				Options.Password.RequireNonAlphanumeric = true;
-				Options.Password.RequiredLength = 8;
-				Options.Password.RequireUppercase = true;
-				Options.Password.RequireLowercase = true;
-				Options.User.RequireUniqueEmail = true;
-				Options.SignIn.RequireConfirmedAccount = true;
-				Options.SignIn.RequireConfirmedEmail = true;
-				Options.SignIn.RequireConfirmedPhoneNumber = false;
-
-
-			})
-				.AddEntityFrameworkStores<AppDbContext>()
-				.AddDefaultTokenProviders();
-
 			var app = builder.Build();
 
+			// Aplica migra√ß√µes
 			using (var scope = app.Services.CreateScope())
 			{
-				var databaseContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-				databaseContext.Database.Migrate();
+				var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+				db.Database.Migrate();
 			}
 
-			// Configure the HTTP request pipeline.
+			// Pipeline HTTP
 			if (!app.Environment.IsDevelopment())
 			{
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
-			app.UseCors("AllowAll");
+			app.UseRouting();
 
+			app.UseCors("AllowFrontend");
 
-
+			app.UseAuthentication();
+			app.UseAuthorization();
 
 			if (app.Environment.IsDevelopment())
 			{
@@ -107,18 +113,10 @@ namespace Teste_tasks
 				app.UseSwaggerUI();
 			}
 
-
-
-			app.UseRouting();
-
-			app.UseAuthentication();
-			app.UseAuthorization();
-
+			// Rotas
 			app.MapControllerRoute(
 				name: "default",
 				pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
 			app.MapControllers();
 
 			app.Run();
